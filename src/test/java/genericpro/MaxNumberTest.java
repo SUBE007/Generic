3b4a2c7aeae286d2cd6c13eb1fa9d1c6c4851e80
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-07 23:51:08 +0530

    added three test cases for returning max float value

--- src/test/java/genericpro/MaxNumberTest.java
+++ src/test/java/genericpro/MaxNumberTest.java
@@ -22,4 +22,23 @@ public class MaxNumberTest {
         Integer largest3= MaxInThreeInt.maxInGivenThreeInteger(100,26,121);
         Assert.assertSame(121,largest3);
     }
+
+    @Test
+    public void givenThreeNumber_IfFindMaximum_ReturnThatValue1() {
+        Float float1= MaxInThreeInt.maxInGivenThreeFloat(125.0f,26.0f,1.0f);
+        Assert.assertEquals(125.0,float1,0.01);
+    }
+
+    @Test
+    public void givenThreeNumber_IfFindMaximum_ReturnThatValue2() {
+        Float float2= MaxInThreeInt.maxInGivenThreeFloat(35.21f,62.5f,12.5f);
+        Assert.assertEquals(62.5,float2,0.01);
+    }
+
+    @Test
+    public void givenThreeNumber_IfFindMaximum_ReturnThatValue3() {
+        Float float3= MaxInThreeInt.maxInGivenThreeFloat(100.0f,26.0f,62.1f);
+        // Assert.assertSame(100.0f,float3);
+        Assert.assertEquals(100,float3,0.01);
+    }
 }
