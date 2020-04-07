6bf3e5b653d555deb9e57418fbf833460ddfae3f
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-08 01:44:54 +0530

    tested all the test cases to hold any no. of argument in method and return maximum

--- src/test/java/genericpro/MaxNumberTest.java
+++ src/test/java/genericpro/MaxNumberTest.java
@@ -7,78 +7,56 @@ import org.junit.Test;
 public class MaxNumberTest {
     @Test
     public void givenNumber_IfFindMaximum_ReturnThatValue1() {
-         Integer largest1= MaxInThreeValue.largestAmongThree(25,26,1);
-         Assert.assertSame(26,largest1);
+        MaxInThreeValue largest2=new  MaxInThreeValue(25,26,1,100);
+        Assert.assertSame(100,largest2.check());
     }
 
     @Test
     public void givenNumber_IfFindMaximum_ReturnThatValue2() {
-        Integer largest2= MaxInThreeValue.largestAmongThree(100,26,0);
-        Assert.assertSame(100,largest2);
+        MaxInThreeValue largest2=new  MaxInThreeValue(100,26,0,400,200);
+        Assert.assertSame(400,largest2.check());
     }
 
     @Test
     public void givenNumber_IfFindMaximum_ReturnThatValue3() {
-        Integer largest3= MaxInThreeValue.largestAmongThree(100,26,121);
-        Assert.assertSame(121,largest3);
+        MaxInThreeValue largest3=new  MaxInThreeValue(500,121);
+        Assert.assertSame(500,largest3.check());
     }
 
     @Test
     public void givenThreeNumber_IfFindMaximum_ReturnThatValue1() {
-        Float float1= MaxInThreeValue.largestAmongThree(125.0f,26.0f,1.0f);
-        Assert.assertEquals(125.0,float1,0.01);
+        MaxInThreeValue largest=new  MaxInThreeValue(125.0f,26.0f,1.0f,200.0f,213.0f);
+        Assert.assertSame(213.0f,largest.check());
     }
 
     @Test
     public void givenThreeNumber_IfFindMaximum_ReturnThatValue2() {
-        Float float2= MaxInThreeValue.largestAmongThree(35.21f,62.5f,12.5f);
-        Assert.assertEquals(62.5,float2,0.01);
+        MaxInThreeValue largest=new  MaxInThreeValue(35.21f,62.5f,12.5f);
+        Assert.assertSame(62.5,largest.check());
     }
 
     @Test
     public void givenThreeNumber_IfFindMaximum_ReturnThatValue3() {
-        Float float3= MaxInThreeValue.largestAmongThree(100.0f,26.0f,62.1f);
-        // Assert.assertSame(100.0f,float3);
-        Assert.assertEquals(100,float3,0.01);
+        MaxInThreeValue largest=new  MaxInThreeValue(100.0f,26.0f,62.1f);
+        Assert.assertSame(100.0,largest.check());
     }
 
     @Test
     public void givenThreeString_IfFindMaximum_ReturnThatString1() {
-       String string1= MaxInThreeValue.largestAmongThree("sube","dube","mube");
-        Assert.assertSame("sube",string1);
+        MaxInThreeValue largest=new  MaxInThreeValue("sube","dube","mube");
+         Assert.assertSame("sube",largest.check());
     }
 
     @Test
     public void givenThreeString_IfFindMaximum_ReturnThatString2() {
-        String string2= MaxInThreeValue.largestAmongThree("dube","lube","hube");
-        Assert.assertSame("lube",string2);
+        MaxInThreeValue largest=new  MaxInThreeValue("dube","lube","hube","Subedar");
+        Assert.assertSame("lube",largest.check());
     }
 
     @Test
     public void givenThreeString_IfFindMaximum_ReturnThatString3() {
-        String string3= MaxInThreeValue.largestAmongThree("quba","lube","sube");
-        Assert.assertSame("sube",string3);
-    }
-
-    @Test
-    public void givenValues_ifFindMaximum_ReturnThatIntegerValue_UsingConstructor() {
-        MaxInThreeValue findMaximum = new MaxInThreeValue<Integer>(456,85,10);
-        Integer largest = (Integer) findMaximum.largestAmongThree();
-        Assert.assertEquals(new Integer(456),largest);
-    }
-
-    @Test
-    public void givenValues_ifFindMaximum_ReturnThatFloatValue_UsingConstructor() {
-        MaxInThreeValue findMaximum = new MaxInThreeValue<Float>(1.1f,20.0f,123.1f);
-        Float largest = (Float) findMaximum.largestAmongThree();
-        Assert.assertEquals(new Float(123.1f),largest,0.1);
-    }
-
-    @Test
-    public void givenValues_ifFindMaximum_ReturnThatStringValue_UsingConstructor() {
-        MaxInThreeValue findMaximum = new MaxInThreeValue<String>("sube","dube","mube");
-        String largest = (String) findMaximum.largestAmongThree();
-        Assert.assertEquals("sube",largest);
+        MaxInThreeValue findMaximum = new MaxInThreeValue("quba","lube","sube","sdf");
+        Assert.assertSame("sube",findMaximum.check());
     }
 
 
