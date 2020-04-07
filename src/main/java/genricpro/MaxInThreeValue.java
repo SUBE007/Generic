e43dfa09b2b1db9a4d0033b3121764d581510d2f
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-08 01:42:31 +0530

    added genric type value to hold any no. of argument in method

--- src/main/java/genricpro/MaxInThreeValue.java
+++ src/main/java/genricpro/MaxInThreeValue.java
@@ -1,28 +1,29 @@
 package genricpro;
 
+import java.util.Arrays;
+
 public class MaxInThreeValue<E extends Comparable> {
-    E firstValue;
-    E secoundValue;
-    E thirdValue;
-    public MaxInThreeValue(E firstValue, E secoundValue, E thirdValue) {
-        this.firstValue = firstValue;
-        this.secoundValue = secoundValue;
-        this.thirdValue = thirdValue;
-    }
+    E []arrValue;
 
-    public <E extends Comparable> E largestAmongThree() {
-
-        E max=(E) largestAmongThree(firstValue,secoundValue,thirdValue);
-        return max;
-    }
-    public static <E extends Comparable<E>> E largestAmongThree(E firstValue, E secoundValue, E thirdValue) {
-        E max=firstValue;
-
-        if(secoundValue.compareTo(max)>0)
-            max=secoundValue;
-        if(thirdValue.compareTo(max)>0) {
-            max=thirdValue;
+    public MaxInThreeValue(){}
+    public MaxInThreeValue(E ...value) {
+        arrValue=(E[]) new Comparable[value.length];
+        E[] array=(E[]) new Comparable[value.length];
+        int i=0;
+        for(E c:value){
+            array[i]=c;
+            i++;
         }
-        return max;
+        this.arrValue = array;
+    }
+     public E check(){
+        Arrays.sort(arrValue);
+        E largestValue=arrValue[arrValue.length-1];
+        printMax(largestValue);
+        return largestValue;
+    }
+
+    public static <E> void printMax(E value){
+        System.out.println(value);
     }
 }
