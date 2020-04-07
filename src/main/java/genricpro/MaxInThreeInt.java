e802b30c4674336e31732aa3e1d60bd8e66e1ce0
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-07 23:04:35 +0530

    added program to find maximum among three number

--- src/main/java/genricpro/MaxInThreeInt.java
+++ src/main/java/genricpro/MaxInThreeInt.java
@@ -1,7 +1,13 @@
 package genricpro;
 
 public class MaxInThreeInt {
-    public static void main(String [] args){
-        System.out.println("Welcome To Generic World!");
+    public static int maxInGivenThreeInteger(Integer num, Integer num2,Integer num3){
+        Integer max=num;
+        if(num2.compareTo(max)>0)
+            max=num2;
+        if(num3.compareTo(max)>0)
+            max=num3;
+        return max;
     }
+
 }
